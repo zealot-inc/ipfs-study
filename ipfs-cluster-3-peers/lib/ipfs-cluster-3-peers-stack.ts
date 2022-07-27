f51abedd60da119af67f2d7e067aade427369f69
Danny Gim <danny.gim@zealot.co.jp>
2022-07-27 13:16:01 +0900

    install dotenv

--- ipfs-cluster-3-peers/lib/ipfs-cluster-3-peers-stack.ts
+++ ipfs-cluster-3-peers/lib/ipfs-cluster-3-peers-stack.ts
@@ -1,16 +1,14 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class IpfsCluster3PeersStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'IpfsCluster3PeersQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // VPC
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+    });
   }
 }
