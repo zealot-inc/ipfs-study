fe547c2c2476e6bfb699c0a73f1ec266b7f5ffda
Danny Gim <danny.gim@zealot.co.jp>
2022-07-27 10:15:38 +0900

    cdk init

---
+++ ipfs-cluster-3-peers/test/ipfs-cluster-3-peers.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as IpfsCluster3Peers from '../lib/ipfs-cluster-3-peers-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ipfs-cluster-3-peers-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new IpfsCluster3Peers.IpfsCluster3PeersStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
